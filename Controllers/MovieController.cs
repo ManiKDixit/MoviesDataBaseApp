678d423c95b5bbf4e9d559800297a9a4b8c4eb5e
ManiKDixit <56781890+ManiKDixit@users.noreply.github.com>
2023-11-04 00:46:55 +0530

    Delete Feature

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -187,6 +187,103 @@ namespace MoviesDataBaseApp.Controllers
 
             return RedirectToAction("Index");
         }
+<<<<<<< Updated upstream
+=======
+
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var curUserId = _httpContextAccessor.HttpContext.User.GetUserId();
+            string appid = curUserId;
+            var movieDetails = await _movieRepository.GetByIdAsync(id,appid);
+            if (movieDetails == null) return View("Error");
+            return View(movieDetails);
+
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+            var curUserId = _httpContextAccessor.HttpContext.User.GetUserId();
+            string appid = curUserId;
+            var movieDetails = await _movieRepository.GetByIdAsync(id,appid);
+            if (movieDetails == null) return View("Error");
+
+            _movieRepository.Delete(movieDetails);
+            return RedirectToAction("Index");
+
+        }
+
+
+
+        //[HttpPost]
+        //public async Task<IActionResult> Edit(int id, EditMovieViewModel movieVM)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        ModelState.AddModelError("", "Failed to edit Movie Details");
+        //        return View("Edit", movieVM);
+        //    }
+
+        //    var userMovie = await _movieRepository.GetByIdAsyncNoTracking(id);
+
+        //    if (userMovie == null)
+        //    {
+        //        return View("Error");
+        //    }
+
+        //    var photoResult = await _photoService.AddPhotoAsync(movieVM.Image);
+
+        //    if (photoResult.Error != null)
+        //    {
+        //        ModelState.AddModelError("Image", "Photo upload failed");
+        //        return View(movieVM);
+        //    }
+
+        //    if (!string.IsNullOrEmpty(userMovie.Image))
+        //    {
+        //        _ = _photoService.DeletePhotoAsync(userMovie.Image);
+        //    }
+
+        //    var movie = new Movie
+        //    {
+        //        Id = id,
+        //        Name = movieVM.Name,
+        //        Description = movieVM.Description,
+        //        Image = photoResult.Url.ToString(),
+        //        Director = movieVM.Director,
+        //        Award = movieVM.Award,
+        //        Studios = movieVM.Studios,
+        //        AppUserId = userMovie.AppUserId,
+        //        //Director = new Director
+        //        //{
+        //        //    Name = movieVM.Director.Name,
+        //        //    Age = movieVM.Director.Age,
+
+        //        //},
+        //        //Award = new Award
+        //        //{
+        //        //    AwardName = movieVM.Award.AwardName,
+        //        //    AwardDate = movieVM.Award.AwardDate,
+        //        //},
+        //        //Studios = new Studios
+        //        //{
+        //        //    StudioName = movieVM.Studios.StudioName,
+        //        //    CEO = movieVM.Studios.CEO,
+        //        //},
+        //        Language = movieVM.Language,
+        //        ReleaseDate = movieVM.ReleaseDate,
+        //        Genre = movieVM.Genre,
+
+        //        //AddressId = clubVM.AddressId,
+        //        //Address = clubVM.Address,
+        //    };
+
+        //    _movieRepository.Update(movie);
+
+        //    return RedirectToAction("Index");
+        //}
+>>>>>>> Stashed changes
     }
 
 }
